54b6b970191a360bd95128ce3833ee03be2bd477
cshiz <64184518+cshiz9206@users.noreply.github.com>
2024-07-19 09:34:34 +0900

    [feat] tour page v.1

--- hellokorea/tour/urls.py
+++ hellokorea/tour/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='tour_index'),
+    path('district/<str:district_name>/', views.district_detail, name='district_detail'),
 ]
--- hellokorea/tour/views.py
+++ hellokorea/tour/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'tour/index.html')
+
+def district_detail(request, district_name):
+    # 해당 구역의 상세 정보를 가져옵니다.
+    context = {
+        'district_name': district_name,
+        # 필요하다면 다른 데이터도 추가
+    }
+    return render(request, 'tour/district_detail.html', context)
