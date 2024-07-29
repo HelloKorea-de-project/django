a0a9077e307d521ba2be56ce32ffb2c0dd41d9e8
cshiz <64184518+cshiz9206@users.noreply.github.com>
2024-07-29 15:53:50 +0900

    [fix] replace CheapestFlight & clean models

--- hellokorea/airline/models.py
+++ hellokorea/airline/models.py
@@ -1,28 +1,9 @@
 from django.db import models
-
-class AirportInformation(models.Model):
-    airportCode = models.CharField(max_length=10, primary_key=True)
-    countryCode = models.CharField(max_length=10, null=False)
-    currencyCode = models.CharField(max_length=10, null=False)
-    createdAt = models.DateTimeField(auto_now_add=True, null=False)
-    updatedAt = models.DateTimeField(auto_now=True, null=False)
-
-class ServiceAirportICN(models.Model):
-    airportCode = models.CharField(max_length=10, primary_key=True)
-    countryCode = models.CharField(max_length=10, null=False)
-    currencyCode = models.CharField(max_length=10, null=False)
-    createdAt = models.DateTimeField(auto_now_add=True, null=False)
-    updatedAt = models.DateTimeField(auto_now=True, null=False)
-    
-class ArrCountToICN(models.Model):
-    airportCode = models.CharField(max_length=10, primary_key=True)
-    count = models.IntegerField(null=False, default=0)
-    createdAt = models.DateTimeField(auto_now_add=True, null=False)
-    updatedAt = models.DateTimeField(auto_now=True, null=False)
 		
 class CheapestFlight(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     depAirportCode = models.CharField(max_length=10, null=False)
+    depCountryCode = models.CharField(max_length=10, null=False)
     currencyCode = models.CharField(max_length=10, null=False)
     arrAirportCode = models.CharField(max_length=10, null=False)
     carrierName = models.CharField(max_length=50, null=True)
